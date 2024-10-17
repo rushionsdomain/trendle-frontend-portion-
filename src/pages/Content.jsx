ee2c5ca652c338a6615e8add3ddba858743ca49e
Gachuri <rushionchegge@gmail.com>
2024-10-17 12:52:42 +0300

    dummy data testing with Ahmed's code

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -5,6 +5,52 @@ const dummyPosts = [
     id: 1,
     username: "user1",
     profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+    imageUrl:
+      "https://i.pinimg.com/474x/48/83/24/4883248276d9d3da8b5ccce7c526d0bd.jpg", // Dummy post image
+    caption: "Enjoying the view!",
+    likes: 150,
+    comments: 20,
+  },
+  {
+    id: 2,
+    username: "user2",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+    imageUrl: "https://via.placeholder.com/500", // Dummy post image
+    caption: "Enjoying the view!",
+    likes: 150,
+    comments: 20,
+  },
+  {
+    id: 3,
+    username: "user1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+    imageUrl: "https://via.placeholder.com/500", // Dummy post image
+    caption: "Enjoying the view!",
+    likes: 150,
+    comments: 20,
+  },
+  {
+    id: 4,
+    username: "user1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+    imageUrl: "https://via.placeholder.com/500", // Dummy post image
+    caption: "Enjoying the view!",
+    likes: 150,
+    comments: 20,
+  },
+  {
+    id: 5,
+    username: "user1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+    imageUrl: "https://via.placeholder.com/500", // Dummy post image
+    caption: "Enjoying the view!",
+    likes: 150,
+    comments: 20,
+  },
+  {
+    id: 6,
+    username: "user1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
     imageUrl: "https://via.placeholder.com/500", // Dummy post image
     caption: "Enjoying the view!",
     likes: 150,
@@ -14,6 +60,31 @@ const dummyPosts = [
 ];
 
 const dummyStories = [
+  {
+    id: 1,
+    username: "user_story1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+  },
+  {
+    id: 2,
+    username: "user_story1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+  },
+  {
+    id: 3,
+    username: "user_story1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+  },
+  {
+    id: 1,
+    username: "user_story1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+  },
+  {
+    id: 1,
+    username: "user_story1",
+    profilePic: "https://via.placeholder.com/50", // Dummy profile picture
+  },
   {
     id: 1,
     username: "user_story1",
